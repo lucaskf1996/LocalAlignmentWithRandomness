47008070049bda4b6f3232dcdfd420964888b6f3
lucaskf1996 <lucaskf1996@gmail.com>
2022-04-28 12:40:36 +0000

    small fixes for output

--- main.cpp
+++ main.cpp
@@ -32,20 +32,22 @@ int main(){
     // std::default_random_engine generator(seed);
     std::uniform_int_distribution<int> distribution2(1,m);
     std::uniform_int_distribution<int> distribution1(1,n);
+    std::string subA, subB;
+    int max_score;
 
     if(n>m){
-        k = distribution2(rd);        
+        k = distribution2(rd);  
+        max_score = -n*2;
     }
     else{
         k = distribution1(rd);
+        max_score = -m*2;
     }
 
-    int max_score = 0;
-    std::string subA, subB;
     std::uniform_int_distribution<int> distribution21(0,m-k);
-    for(int rounds = 0; rounds<1000; rounds++){
+    for(int rounds = 0; rounds<100000; rounds++){
         p = distribution2(rd);
-        int scoreList[p];
+        // int scoreList[p];
         subA = a.substr(0,k);
         for(int i = 0; i < p; i++){
             score = 0;
@@ -54,18 +56,21 @@ int main(){
             for(int index = 0; index < k; index++){
                 score += checkPair(subA[index], subB[index], wmat, wmis, score);
             }
-            scoreList[i] = score;
+            // scoreList[i] = score;
             if(score > max_score){
                 max_score = score;
                 bestA = subA;
                 bestB = subB;
             }
-            std::cout << subA << std::endl << subB << std::endl;
-            std::cout << score << std::endl;
+            // std::cout << subA << std::endl << subB << std::endl;
+            // std::cout << score << std::endl;
         }
     }
-    std::cout << std::endl << "n: " << n << std::endl << "m: " << m << std::endl << "k: " << k << std::endl << "p: " << p << std::endl;
-    std::cout << "max_score: " << max_score << std::endl;
-    std::cout << "bestA: " << bestA << std::endl;
-    std::cout << "bestB: " << bestB << std::endl;
+    // std::cout << std::endl << "n: " << n << std::endl << "m: " << m << std::endl << "k: " << k << std::endl << "p: " << p << std::endl;
+    // std::cout << "max_score: " << max_score << std::endl;
+    // std::cout << "bestA: " << bestA << std::endl;
+    // std::cout << "bestB: " << bestB << std::endl;
+    std::ofstream outfile ("output.txt");
+    outfile << max_score << std::endl << "bestA: " << bestA << std::endl << "bestB: " << bestB << std::endl;
+    outfile.close();
 }
